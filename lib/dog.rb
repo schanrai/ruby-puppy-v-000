fa96296b0ec6cceb5dea82a7f626a8dfa533adc5
Sushi Chanrai <sushi@pathworks.co>
2018-07-12 18:44:04 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+class Song
+ 
+  @@all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    @@all.each do |song|
+      puts song.name
+    end
+  end
+end
+
+=end
