61298513368ed3942bb663b3ff4b788b00f8121d
Sushi Chanrai <sushi@pathworks.co>
2018-07-12 19:05:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,19 +20,3 @@ end
 
 
 
-#class Song
-
-#  @@all = []
-
-#  attr_accessor :name
-
-#  def initialize(name)
-#    @name = name
-#  end
-
-#  def self.all
-#    @@all.each do |song|
-#      puts song.name
-#    end
-#  end
-# end
