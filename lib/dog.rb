3241f8fa5c9ec5fe09afdeb3f1a6c61ab526696c
Sushi Chanrai <sushi@pathworks.co>
2018-07-12 19:07:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,17 @@ class Dog
   @@all = []
   attr_accessor :name
 
-  def initialize(name)
+    def initialize(name)
       @name = name
       @@all << self
     end
+
+    def self.all
+      @all.map {|dog| dog.name}
+    end
+
+    #You will need to write a class method, `.all`, that iterates over all of the individual dogs stored in the `@@all` array 
+    #and `puts` out their name to the terminal. 
 
 
 end
