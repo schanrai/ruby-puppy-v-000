fda2e88d29634e9487c690ef1ee43b98976f5a13
Sushi Chanrai <sushi@pathworks.co>
2018-07-12 19:03:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,23 +17,20 @@ end
 
 
 
-=begin
-class Song
 
-  @@all = []
+#class Song
 
-  attr_accessor :name
+#  @@all = []
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+#  attr_accessor :name
 
-  def self.all
-    @@all.each do |song|
-      puts song.name
-    end
-  end
-end
+#  def initialize(name)
+#    @name = name
+#  end
 
-=end
+#  def self.all
+#    @@all.each do |song|
+#      puts song.name
+#    end
+#  end
+# end
